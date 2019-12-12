e6007818da3bb349e6a1caebed29eb15a4628aa6
Chris Underwood <cunderwoodmn@gmail.com>
2019-12-12 16:53:04 -0500

    more layout of FindItem. Minor fixes to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import FindItem from './FindItem';
 import './App.css';
 
+
 function App() {
+
+
+
   return (
     <div >
 
--- src/FindItem.js
+++ src/FindItem.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class FindItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      barcode: "1001",
+      item: { title: "Nothing honey." },
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.prevState.barcode !== this.state.barcode) {
+
+    }
+  }
 
   render() {
-    return <h1>FindItem.js</h1>
+    return (
+      <div>
+        <h1>FindItem.js</h1>
+        <p>{this.state.barcode}</p>
+        <p>{this.state.item.title}</p>
+      </div>)
   }
 }
 
