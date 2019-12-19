22549d39851de822286314c853f44c371c57d6c0
Chris Underwood <cunderwoodmn@gmail.com>
2019-12-19 09:18:00 -0500

    front end code for application. React app that calls our backend.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReceiveBarcode from './ReceiveBarcode';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Brandeis Library Inventory App</h1>
+      < ReceiveBarcode />
+    </div >
+  );
+}
+
+export default App;
