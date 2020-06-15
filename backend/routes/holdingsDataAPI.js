0699f8a7e4f6004ed95d9402ce78427e83c1a574
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-15 14:53:27 -0400

    add xmlpath and xpath to path. Started conversion of holdingsDataAPI toxml reading of data

--- backend/routes/holdingsDataAPI.js
+++ backend/routes/holdingsDataAPI.js
@@ -1,40 +1,63 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
 
 //This route is here to allow people to make sure the back end is running via the browser.
 router.get('/', function (req, res, next) {
   try {
-    res.status(200).send("You have reached route holdingsData")
+    res.status(200).send('You have reached route holdingsData');
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-})
+});
 
 //route to retrieve the holdings for an item so that the shelf location can be retreived displaye.
-router.put("/", async function (req, res, next) {
+router.put('/', async function (req, res, next) {
   try {
     //item retrieve query to Alma backend. Holdings API URL, Item Barcode, and APIKEY
-    const { data } = await axios.get(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_API_PATH_HOLDINGS + req.body.mmsId + '/holdings/' + req.body.holdingId + '?apikey=' + process.env.EXLIBRIS_API_BIB_GET_KEY);
+    const { data } = await axios.get(
+      process.env.EXLIBRIS_API_ROOT +
+        process.env.EXLIBRIS_API_PATH_HOLDINGS +
+        req.body.mmsId +
+        '/holdings/' +
+        req.body.holdingId +
+        '?apikey=' +
+        process.env.EXLIBRIS_API_BIB_GET_KEY
+    );
 
     //pulling out the XML hodings data and searching to see if 'committed to retain' is in the text. If the text is found returning the text to the front end to show.
-    let xmlData = data.anies[0];
-    let string583a = ""
-    const dataJSON = JSON.stringify(xmlData);
-    const xmlNum = dataJSON.indexOf('committed to retain')
-
-    if (xmlNum >= 0) {
-      string583a = 'committed to retain';
+    let string583a = '';
+    let document = data.anies[0];
+    document = document.toString();
+    console.log('document', document);
+    let xmlParsedDoc = await new dom().parseFromString(document);
+    string583a = xpath.select('//datafield[@tag=583]/subfield', xmlParsedDoc);
+    if (!string583a) {
+      string583a = '';
     } else {
-      string583a = "";
+      string583a = string583a[0].toString();
     }
 
+    console.log('string583a----------   ', string583a);
     res.send(string583a);
+    // let xmlData = data.anies[0];
+    // let string583a = '';
+    // const dataJSON = JSON.stringify(xmlData);
+    // const xmlNum = dataJSON.indexOf('committed to retain');
+
+    // if (xmlNum >= 0) {
+    //   string583a = 'committed to retain';
+    // } else {
+    //   string583a = '';
+    // }
+
+    // res.send(string583a);
   } catch (error) {
-    console.log("retreiveItemErrorAPI Error:   ", error.message);
+    console.log('retreiveItemErrorAPI Error:   ', error.message);
     res.send(error);
   }
 });
